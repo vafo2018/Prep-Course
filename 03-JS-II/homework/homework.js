22bea74d9fc56c1cd365c04e31fd7e5d8e1bf50b
vafo2018 <fabricioorelia@hotmail.com>
2022-08-30 11:45:22 -0300

    JSII Tarea 30082022

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -179,8 +179,19 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-
-
+ if(numero === 0 || numero === 1){
+   return false;
+ }
+ var encontre = false;
+ while(encontre){
+  var numprueba = Math.random();
+  if (numprueba !== 0 && numprueba !== 1){
+    encontre = true;
+  }
+if (numero % numprueba === 0 )
+  return false;
+ }
+ return true;
 }
 
 function esVerdadero(valor){
